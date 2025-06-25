507004b3b36ad7a7f897b92b02574277d30ce4ed
Ms. Imperfectionist <85920051+MsImperfect@users.noreply.github.com>
2025-06-26 00:46:23 +0530

    feat: sound manager created

---
+++ SpaceInvaders/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public enum Sound
+    {
+        PowerUp,
+        BgMusic,
+        Explosion,
+    }
+    public static SoundManager Instance { get; private set; }
+    public SoundAudioClip[] soundAudioClips;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance == this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySound(Sound sound, Vector3 position)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        soundGameObject.transform.position = position;
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.clip = GetAudioClip(sound);
+        audioSource.Play();
+    }
+
+    public void PlaySound(Sound sound)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
+    public static AudioClip GetAudioClip(Sound sound)
+    {
+        foreach(SoundAudioClip clip in SoundManager.Instance.soundAudioClips)
+        {
+            if(clip.sound == sound)
+            {
+                return clip.audioClip;
+            }
+        }
+        return null;
+    }
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public Sound sound;
+        public AudioClip audioClip;
+    }
+}
