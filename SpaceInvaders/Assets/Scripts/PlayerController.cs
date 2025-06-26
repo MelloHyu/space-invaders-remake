aace94ea92433b44d477933670f4471d197aa0ca
Dhruv-mech <khandelwaldhruv0211@gmail.com>
2025-06-26 15:36:30 +0530

    Updates

--- SpaceInvaders/Assets/Scripts/PlayerController.cs
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
 
+    private InputSystem_Actions inputActions;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.transform.position += Vector3.left * this.playerSpeed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.position += Vector3.right * this.playerSpeed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            this.transform.position += Vector3.down * this.playerSpeed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            this.transform.position += Vector3.up * this.playerSpeed * Time.deltaTime;
-        }
-
+        // Only horizontal movement is considered
+        Vector3 moveDirection = new Vector3(movement.x, movement.y, 0);
+        transform.position += moveDirection * playerSpeed * Time.deltaTime;
     }
 }
- 
