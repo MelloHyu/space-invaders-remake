2e551408fd5fd7918adf9b3652b2ea0aaebfd896
Om Gupta <guptaom750@gmail.com>
2025-07-14 00:38:55 +0530

    ok

---
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+
+    private InputSystem_Actions inputActions;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        // Only horizontal movement is considered
+        Vector3 moveDirection = new Vector3(movement.x, movement.y, 0);
+        if (transform.position.x < -25 || transform.position.x > 25)
+        {
+            return;
+        }
+        transform.position += moveDirection * playerSpeed * Time.deltaTime;
+
+        
+    }
+}
